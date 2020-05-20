a5aab0e057d0e1abdb2bfcc9d01ebb07da0033a4
dherrada <=>
2020-05-20 18:00:27 -0400

    Fixed doxygen index page

--- Adafruit_EEPROM_I2C.cpp
+++ Adafruit_EEPROM_I2C.cpp
@@ -1,20 +1,20 @@
 /**************************************************************************/
 /*!
-    @file     Adafruit_EEPROM_I2C.cpp
-    @author   KTOWN (Adafruit Industries)
-
-    Driver for the Adafruit I2C EEPROM breakout.
-
-    Adafruit invests time and resources providing this open source code,
-    please support Adafruit and open-source hardware by purchasing
-    products from Adafruit!
+ * @file     Adafruit_EEPROM_I2C.cpp
+ * @author   KTOWN (Adafruit Industries)
+ *
+ * Driver for the Adafruit I2C EEPROM breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
  *
  * 	Adafruit invests time and resources providing this open source code,
  *  please support Adafruit and open-source hardware by purchasing products from
  * 	Adafruit!
  *
  *	BSD license (see license.txt)
-*/
+ */
 /**************************************************************************/
 #include <math.h>
 #include <stdlib.h>
--- Adafruit_EEPROM_I2C.h
+++ Adafruit_EEPROM_I2C.h
@@ -12,14 +12,22 @@
  *	BSD license (see license.txt)
  */
 /**************************************************************************/
+/// @cond DISABLE
 #ifndef _ADAFRUIT_EEPROM_I2C_H_
+/// @endcond
 #define _ADAFRUIT_EEPROM_I2C_H_
 
+/// @cond DISABLE
 #if ARDUINO >= 100
+/// @endcond
 #include <Arduino.h>
+/// @cond DISABLE
 #else
+/// @endcond
 #include <WProgram.h>
+/// @cond DISABLE
 #endif
+/// @endcond
 
 #include <Wire.h>
 
@@ -42,4 +50,6 @@ private:
   TwoWire *_wire;
 };
 
+/// @cond DISABLE
 #endif
+/// @endcond
