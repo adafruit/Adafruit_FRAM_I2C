b54c7d849d24687542fb7cf4908ff33288cc3776
ladyada <support@adafruit.com>
2021-07-01 13:59:09 -0400

    update example!

--- examples/Generic_I2C_EEPROM/Generic_I2C_EEPROM.ino
+++ examples/Generic_I2C_EEPROM/Generic_I2C_EEPROM.ino
@@ -1,4 +1,3 @@
-#include <Wire.h>
 #include "Adafruit_EEPROM_I2C.h"
 
 /* Example code for the Adafruit I2C EEPROM breakout */
@@ -26,11 +25,45 @@ void setup(void) {
   uint8_t test = i2ceeprom.read(0x0);
   Serial.print("Restarted "); Serial.print(test); Serial.println(" times");
   // Test write ++
-  i2ceeprom.write(0x0, test+1);
-  
-  // dump the first 256 bytes of memory
+  test++;
+  i2ceeprom.write(0x0, test);
+
+  // Try to determine the size by writing a value and seeing if it changes the first byte
+  Serial.println("Testing size!");
+
+  uint32_t max_addr;
+  for (max_addr = 1; max_addr < 0xFFFF; max_addr++) {
+    if (i2ceeprom.read(max_addr) != test)
+      continue; // def didnt wrap around yet
+
+    // maybe wraped? try writing the inverse
+    if (! i2ceeprom.write(max_addr, (byte)~test)) {
+        Serial.print("Failed to write address 0x");
+        Serial.println(max_addr, HEX);
+    }
+
+    // read address 0x0 again
+    uint8_t val0 = i2ceeprom.read(0);
+
+    // re-write the old value
+    if (! i2ceeprom.write(max_addr, test)) {
+        Serial.print("Failed to re-write address 0x");
+        Serial.println(max_addr, HEX);
+    }    
+
+    // check if addr 0 was changed
+    if (val0 == (byte)~test) {
+      Serial.println("Found max address");
+      break;
+    }
+  }
+  Serial.print("This EEPROM can store ");
+  Serial.print(max_addr);
+  Serial.println(" bytes");
+    
+  // dump the memory
   uint8_t val;
-  for (uint16_t addr = 0; addr < 256; addr++) {
+  for (uint16_t addr = 0; addr < max_addr; addr++) {
     val = i2ceeprom.read(addr);
     if ((addr % 32) == 0) {
       Serial.print("\n 0x"); Serial.print(addr, HEX); Serial.print(": ");
