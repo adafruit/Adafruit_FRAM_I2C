407dc9b45f8e056df4abee85f0b485a3a4f7a5d4
Doxygen CI <ci-arduino@invalid>
2021-07-01 18:29:22 +0000

    Deploy docs to GitHub Pages from commit c86fda8e54
    Commit: c86fda8e544cac6dad3ce1c763a4bb5f2316382f
    GitHub Actions run: 991109543

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['read',['read',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a30d1b155898db481b91fd482065aacbb',1,'Adafruit_EEPROM_I2C']]],
-  ['read8',['read8',['../class_adafruit___f_r_a_m___i2_c.html#a6fd1a7c80b0b65e81e854a19df7d4124',1,'Adafruit_FRAM_I2C']]]
+  ['read8',['read8',['../class_adafruit___f_r_a_m___i2_c.html#a6fd1a7c80b0b65e81e854a19df7d4124',1,'Adafruit_FRAM_I2C']]],
+  ['readobject',['readObject',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a81ed680b7c33b666b28084abe10b1c12',1,'Adafruit_EEPROM_I2C']]]
 ];
