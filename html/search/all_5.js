8f127debd03ac464403ef8afa476b502c87b93ad
Doxygen CI <ci-arduino@invalid>
2021-07-01 19:11:01 +0000

    Deploy docs to GitHub Pages from commit c4be8f0828
    Commit: c4be8f08284faf03d8efe08dfc507332d4aee77d
    GitHub Actions run: 991217064

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['read',['read',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a30d1b155898db481b91fd482065aacbb',1,'Adafruit_EEPROM_I2C']]],
+  ['read',['read',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a30d1b155898db481b91fd482065aacbb',1,'Adafruit_EEPROM_I2C::read(uint16_t addr)'],['../class_adafruit___e_e_p_r_o_m___i2_c.html#ad882f121e1382b805aef0dbba5eb41ba',1,'Adafruit_EEPROM_I2C::read(uint16_t addr, uint8_t *buffer, uint16_t num)']]],
   ['read8',['read8',['../class_adafruit___f_r_a_m___i2_c.html#a6fd1a7c80b0b65e81e854a19df7d4124',1,'Adafruit_FRAM_I2C']]],
   ['readobject',['readObject',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a81ed680b7c33b666b28084abe10b1c12',1,'Adafruit_EEPROM_I2C']]]
 ];
