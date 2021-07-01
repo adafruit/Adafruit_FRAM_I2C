2a8e039d5b5770a063004ace362361571d064eb1
Doxygen CI <ci-arduino@invalid>
2021-07-01 18:08:45 +0000

    Deploy docs to GitHub Pages from commit f86ddd50f7
    Commit: f86ddd50f7c1e91796f1f11cb16d2d6d33a6a02b
    GitHub Actions run: 991052784

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['write8',['write8',['../class_adafruit___e_e_p_r_o_m___i2_c.html#ac2cd5aa1f02187079bc8ba98174eba95',1,'Adafruit_EEPROM_I2C::write8()'],['../class_adafruit___f_r_a_m___i2_c.html#a6f58ddf3e2f76b63df8165b978f5761c',1,'Adafruit_FRAM_I2C::write8()']]]
+  ['write',['write',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a0a6754e36fa9b511f597a9827b4b11db',1,'Adafruit_EEPROM_I2C']]],
+  ['write8',['write8',['../class_adafruit___f_r_a_m___i2_c.html#a6f58ddf3e2f76b63df8165b978f5761c',1,'Adafruit_FRAM_I2C']]]
 ];
--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['read8',['read8',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a12a739a894aad4a443705caa025cd204',1,'Adafruit_EEPROM_I2C::read8()'],['../class_adafruit___f_r_a_m___i2_c.html#a6fd1a7c80b0b65e81e854a19df7d4124',1,'Adafruit_FRAM_I2C::read8()']]]
+  ['read',['read',['../class_adafruit___e_e_p_r_o_m___i2_c.html#a30d1b155898db481b91fd482065aacbb',1,'Adafruit_EEPROM_I2C']]],
+  ['read8',['read8',['../class_adafruit___f_r_a_m___i2_c.html#a6fd1a7c80b0b65e81e854a19df7d4124',1,'Adafruit_FRAM_I2C']]]
 ];
